c470b159bef1af6765456927f5ad17410cf7761a
wesleyhuang23 <wesleyhuang23@gmail.com>
2017-01-29 12:42:13 -0800

    function definitions and invoking functions within functions

--- looping.py
+++ looping.py
@@ -101,3 +101,79 @@ def compute_bill(food):
             total = total + prices[key]
         stock[key] = stock[key] - 1
     return total
+
+
+
+
+#MORE lists in ENTRIES or DICTRIONARIES
+lloyd = {
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+alice = {
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+tyler = {
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
+
+students = [lloyd, alice, tyler]
+
+for student in students:
+    print student['name']
+    print student['homework']
+    print student['quizzes']
+    print student['tests']
+
+#-printing students dictionary
+
+#average function
+#-use built in functions
+def average(numbers):
+    total = sum(numbers)
+    total = float(total)
+    total = total / len(numbers)
+    return total
+
+#weighted items
+#-takes in the average and then multiplies the weight and sums them
+def get_average(student):
+    homework = average(student["homework"])
+    quizzes = average(student["quizzes"])
+    tests = average(student["tests"])
+    return 0.6 * tests + 0.3 * quizzes + 0.1 * homework
+
+#getting the letter grade
+def get_letter_grade(score):
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"
+
+get_letter_grade(lloyd)
+
+#getting the average of the class
+#-loops through to get their average by invoking the get average function and then pushes it into the results list
+def get_class_average(students):
+    results = []
+    for student in students:
+        results.append(get_average(student))
+    return sum(results) / len(results)
+
+#print the results
+print get_class_average(students)
+print get_letter_grade(get_class_average(students))
