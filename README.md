54ee6cf85272bc5984c730ba00d63e083acbb8d1
wesleyhuang23 <wesleyhuang23@gmail.com>
2017-02-01 17:20:16 -0800

    Ruby formmater

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Programming Lessons
 -Python
+-Ruby
+-PHP
--- ruby/1. intro.rb
+++ ruby/1. intro.rb
@@ -36,3 +36,27 @@ quotient = 13209 / 17
 #everything in Ruby is an object so you can call multiple methods
 name = "Wesley".downcase.reverse.upcase
 name.method1.method2.method3
+
+
+#gets.chomp is prompt
+#{} is reading the value of the variable
+#! will change the actual value without having you to put it in a new variable
+firstname = "Sam"
+puts "#{firstname}"
+puts "my name is #{first_name}"
+.capitalize!
+
+lastname = "Smith"
+puts "#{lastname}"
+puts "my last name is #{last_name}"
+.capitalize!
+
+city = "Los Angeles"
+puts "#{city}"
+puts "I live in #{city}"
+.capitalize!
+
+state = "California"
+puts "#{state}"
+puts "I live in the city of #{state}"
+.upcase!
